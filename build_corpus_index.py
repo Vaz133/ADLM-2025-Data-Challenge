ceab872b77b66e581ca0c5a0f9591d9ec7410775
Azimi <vazimi@alamedahealthsystem.org>
2025-08-08 15:06:47 -0700

    added build_corpus_index

--- build_corpus_index.py
+++ build_corpus_index.py
@@ -22,3 +22,28 @@ INDEX_DIR = "corpus_index"
 def extract_text(path):
     doc = fitz.open(path)
     return "\n".join(page.get_text() for page in doc)
+
+## Function to build the corpus index
+# This function processes all PDF files in the specified directory, extracts text, splits it into chunks, and returns a list of Document objects.
+def build_corpus_index(path):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    documents = []
+
+    for file in os.listdir(CORPUS_DIR):
+        if file.endswith(".pdf"):
+            path = os.path.join(CORPUS_DIR, file)
+            print(f"Processing: {file}")
+            text = extract_text(path)
+            chunks = splitter.split_text(text)
+            for chunk in chunks:
+                documents.append(Document(page_content=chunk, metadata={"source": file}))
+    return documents
+
+
+## Main function to build the corpus index
+def main():
+    path = r'C:\Users\vazimi\Desktop\Sandbox\ADLM-2025-Data-Challenge'
+    documents=build_corpus_index(path)
+
+if __name__ == "__main__":
+    main()
